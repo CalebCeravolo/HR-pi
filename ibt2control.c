3bd6eb12f380011e0e2256469984fa1c11d5d10f
ykuge <ykuge@uw.edu>
2025-10-30 19:38:33 -0700

    Update ibt2control.c

--- ibt2control.c
+++ ibt2control.c
@@ -5,12 +5,38 @@
 // control input level 3.3-5V
 // working duty cycle 0-100%
 
+#define RPWM 32
+#define LPWM 33
+#define R_EN 36
+#define L_EN 37
+
+
+void main() {
+  wiringPiSetup();
+  pinMode(R_EN, HIGH);
+  pinMode(L_EN, LOW);
+  spinOneWay();
+  delay(2000);
+  spinOtherWay();
+  delay(2000);
+  return 0;
+}
+
+void spinOneWay(){
+  pinMode(RPWM, 500);
+  pinMode(LPWM, 0);
+}
+
+void spinOtherWay(){
+  pinMode(LPWM, 500);
+  pinMode(RPWM, 0);
+}
 
 
 
-int RWPM = 3; 
-int LPWM = 5;
 
+
+/*
 void setup() {
   pinMode(RWPM, OUTPUT);
   pinMode(LWPM, OUTPUT);
@@ -28,3 +54,4 @@ void spinOneWay(){
 void spinOpposite(){
 
 }
+*/
