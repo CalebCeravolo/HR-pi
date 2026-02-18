9b468708ddc7d4c839a9bc5c963ba60742388b9c
CalebCeravolo <ceravcal@uw.edu>
2026-02-17 19:47:53 -0800

    Puins

--- C_Code/pins.h
+++ C_Code/pins.h
@@ -1,24 +1,27 @@
-#define LOAD5_1_1 26
-#define LOAD5_1_2 19
-#define LOAD5_1_3 13
+#define LOAD5_1_1 26 // Pump
+#define LOAD5_1_2 19 // Pump
+#define LOAD5_1_3 13 // Stir
 
-#define LOAD5_2_1 6
-#define LOAD5_2_2 5
-#define LOAD5_2_3 0
+#define LOAD5_2_1 6 // Pump
+#define LOAD5_2_2 5 // Pump
+#define LOAD5_2_3 0 // Pump
 
 #define LOAD12_1 22 // Fan1
 #define LOAD12_2 27 // Fan2
 #define LOAD12_3 17 // Heater
 
-#define H1A_1 24
+#define H1A_1 24 // Fluids Pump
 #define H1A_2 23
-#define H1A_3 18
+#define H1A_3 18 // Spin
 #define H1A_4 15
 
-#define H5A_1_1 21
+#define H5A_1_1 21 // Raise Lower Column
 #define H5A_1_2 20
-#define H5A_2_1 16
+
+#define H5A_2_1 16 // Centrifuge
 #define H5A_2_2 12
-#define H5A_3_1 1
+
+#define H5A_3_1 1 // Raise Lower
 #define H5A_3_2 7
+
 
