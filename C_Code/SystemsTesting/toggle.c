2e7095e75e7006ec2c92f8b030e2209477ba4512
HR-Pi-Admin <instrumentationsubsystem@gmail.com>
2026-02-19 19:02:29 -0800

    Update Documentation

--- C_Code/SystemsTesting/toggle.c
+++ C_Code/SystemsTesting/toggle.c
@@ -1,4 +1,3 @@
-
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
 // #include <stdio.h>
@@ -10,19 +9,13 @@ int main(int argc, char *argv[]) {
     wiringPiSetupPinType(WPI_PIN_WPI);
     int vals[argc-1];
     intparse(argc-1, argv+1, vals);
-    if (vals[0]==1){
-        getAlt(pin);
-        
+    int mode = getAlt(vals[0]);
+    if (mode==1){
+        digitalWrite(vals[0], ~digitalRead(vals[0]));
+        return 0;
     } else {
-        
+        pinMode(vals[0], OUTPUT);
+        digitalWrite(vals[0], 1);
+        return 0;
     }
-    if (argc>1){
-        
-    }
-    // sleep(1);
-    // digitalWrite(RIGHTEN, 0);
-    // digitalWrite(LEFTEN, 1);
-    // sleep(5);
-    // pinMode(LEFTEN, PM_OFF);
-    // pinMode(RIGHTEN, PM_OFF);
 }
--- C_Code/functions.h
+++ C_Code/functions.h
@@ -7,73 +7,22 @@
 #include <wiringPiSPI.h>
 
 // Function declarations
-/*
-Converts a string to an integer
-arg: The pointer to the string
-output: int value of string
-*/
-extern int char_to_int(char * arg);
 
+extern int char_to_int(char * arg);
 extern float char_to_float(char * arg);
-/*
-Prints the binary representation of a number 
-len: Length of output binary number
-in: Number to print as binary
-*/
 extern void print_bin(int len, int in);
-/*
-Prints an array of byte length len in hexidecimal 
-input: The input array
-len: Byte length of array
-*/
 extern void print_arr(void * input, int len);
-/*
-Turns a list of args into ints
-Inputs: 
-argc: length of args
-args: list of strings
-output: int list to be overwritten
-*/
 extern void intparse(int argc, char** args, int * output);
 extern void floatparse(int argc, char** args, float * output);
-/*
-Converts a uint32 number to a byte array
-*/
 void to_char_array(uint32_t base, unsigned char * output);
-
-/*
-Creates a pwm signal on the fpga
-motor: motor pin to interface with
-pwm_period: 0-2000 value representing duty cycle
-*/
+extern uint32_t fpga_command(uint32_t command);
 extern uint32_t fpga_pwm_uptime(uint8_t motor, uint32_t pwm_uptime);
-extern uint32_t fpga_pwm_period(uint8_t motor, uint32_t pwm_uptime);
-/*
-Sends raw data out. Not recommended for use
-*/
+extern uint32_t fpga_pwm_period(uint8_t motor, uint32_t pwm_period);
 extern uint32_t fpga_raw(uint32_t outgoing);
-
-/*
-Requests the return of data by sending the address of the data
-*/
 extern uint32_t fpga_datatran(uint8_t data_addr);
-
-/*
-Requests then returns data from FPGA
-*/
 extern uint32_t fpga_safetran(uint8_t data_addr);
-/*
-Fast version of data transfer. Manual opening and closing of spi channel needed
-*/
 void fpga_fasttran(uint8_t data_addr, uint32_t* result);
-
-/*
-Converts a character array to a uint32 value
-*/
 extern uint32_t to_uint_value(unsigned char * input);
-/*
-Returns length of a character string
-*/
 extern int length_of(char * point);
 
 #endif // FPGA_UTILS_H
