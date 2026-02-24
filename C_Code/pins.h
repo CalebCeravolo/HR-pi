ca5824d0f76d7f448afd3f514b3ce5ab91bef258
HR-pi-Admin <instrumentationsubsystem@gmail.com>
2026-02-23 19:04:27 -0800

    joisjdf

--- C_Code/pins.h
+++ C_Code/pins.h
@@ -8,20 +8,20 @@
 
 #define LOAD12_1 3 // Fan1
 #define LOAD12_2 2 // Fan2
-#define LOAD12_3 0 // Heater
+#define LOAD12_3 0 // Centrifuge
 
 #define H1A_1 5 // Fluids Pump
 #define H1A_2 4
 #define H1A_3 1 // Spin
 #define H1A_4 16
 
-#define H5A_1_1 29 // Raise Lower Column
-#define H5A_1_2 28
+#define H42A_1 11 // Raise Lower Column
+#define H42A_2 31
 
-#define H5A_2_1 27 // Centrifuge
-#define H5A_2_2 26
+// #define H18A_2_1 27 // Centrifuge
+// #define H18A_2_2 26
 
-#define H5A_3_1 31 // Raise Lower
-#define H5A_3_2 11
+#define H18A_3_1 8 // Raise Lower
+#define H18A_3_2 9
 
 
---
+++ Executables/startup.sh
@@ -0,0 +1,25 @@
+set 25 0 # Pump
+set 24 0 # Pump
+set 23 0 # Stir
+
+set 22 0 # Pump
+set 21 0 # Pump
+set 30 0 # Pump
+
+set 3 1 # Fan1
+set 2 1 # Fan2
+set 0 0 # Heater
+
+set 5 0 # Fluids Pump
+#define H1A_2 4
+#define H1A_3 1 # Spin
+#define H1A_4 16
+
+#define H42A_1 29 # Raise Lower Column
+#define H42A_2 28
+
+#define H18A_2_1 27 # Centrifuge
+#define H18A_2_2 26
+
+#define H18A_3_1 8 # Raise Lower
+#define H18A_3_2 9
