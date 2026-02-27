73025ccdccb625f5b8128c2cffbf9ca6a0a1d81d
HR-Pi-Admin <instrumentationsubsystem@gmail.com>
2026-02-26 17:45:00 -0800

    Update i2c

--- C_Code/SystemsTesting/i2c.c
+++ C_Code/SystemsTesting/i2c.c
@@ -1,16 +1,19 @@
 #include <wiringPi.h>
+#include <wiringPiI2C.h>
 #include "../functions.h"
 #define DEVICE_ID 0x39
 #define COMMAND_REGISTER_BIT 0x80
 #define MULTI_BYTE_BIT 0x20
-void main(int argc, char *argv[]){
-    int[argc-1] vals;
-    intparse(argc, argv, output)
+int main(int argc, char *argv[]){
+    int vals[argc-1];
+    intparse(argc, argv, vals);
     int fd = wiringPiI2CSetup(DEVICE_ID);
     if (fd == -1) {
         printf("Failed to init I2C communication.\n");
         return -1;
     }
     wiringPiI2CWriteReg8(fd, COMMAND_REGISTER_BIT, 0b00000011);
-    wiringPiI2CReadReg8(fd, )
+    int result = wiringPiI2CReadReg8(fd, COMMAND_REGISTER_BIT | MULTI_BYTE_BIT | vals[0]);
+    printf("Result: %d", result);
+    return 0;
 }
