778d44bc67701908505dfb6eedc644a5f8647930
henryksz-uw <henryksz@uw.edu>
2026-04-25 14:07:55 -0700

    from degrees to cm

--- C_Code/SystemsTesting/Column/raiselower_column.c
+++ C_Code/SystemsTesting/Column/raiselower_column.c
@@ -6,26 +6,36 @@
 #include <wiringPiSPI.h>
 #include "../../functions.h"
 #include "../../pins.h"
-#define CPR 663.0 / 6
+
 #define DATA_ADDR ENC_RAISE_LOWER
+
+/*
+ * Set TICKS_OVER_REFERENCE to that count and REFERENCE_DISTANCE_CM to the distance you moved.
+ * The values are different for each column because the linear axis is different.
+ * TICKS_PER_CM is derived; all targets use centimeters.
+ */
+#define REFERENCE_DISTANCE_CM 10.0f
+#define TICKS_OVER_REFERENCE 1000.0f /* replace with measured ticks for REFERENCE_DISTANCE_CM */
+#define TICKS_PER_CM (TICKS_OVER_REFERENCE / REFERENCE_DISTANCE_CM)
+#define CM_TOLERANCE 0.05f //how close to the target we want to get to before stopping (momentum of the motor)
 
 static int sigint = 0;
 static void intHandler(int dummy) { sigint = 1; }
 
-static void enc_dec(int32_t *amount, float *degrees) {
-  *amount = fpga_safetran(DATA_ADDR);
-  *degrees = (*amount * 360.0) / CPR;
+static void read_position(int32_t *ticks, float *cm) {
+  *ticks = fpga_safetran(DATA_ADDR);
+  *cm = (float)(*ticks) / TICKS_PER_CM;
 }
 
-static int raiseLowerTo(float target, int raise_pin, int lower_pin) {
+static int raiseLowerTo(float target_cm, int raise_pin, int lower_pin) {
   pinMode(raise_pin, OUTPUT);
   pinMode(lower_pin, OUTPUT);
 
-  int32_t amount;
-  float degrees;
-  enc_dec(&amount, &degrees);
-  float distance_remaining = fabsf(degrees - target);
-  if (degrees > target) {
+  int32_t ticks;
+  float cm;
+  read_position(&ticks, &cm);
+  float distance_remaining = fabsf(cm - target_cm);
+  if (cm > target_cm) {
     digitalWrite(raise_pin, 0);
     digitalWrite(lower_pin, 1);
   } else {
@@ -33,19 +43,20 @@ static int raiseLowerTo(float target, int raise_pin, int lower_pin) {
     digitalWrite(lower_pin, 0);
   }
 
-  while (distance_remaining > 5) {
+  while (distance_remaining > CM_TOLERANCE) {
     if (sigint) {
       digitalWrite(raise_pin, 0);
       digitalWrite(lower_pin, 0);
       break;
     }
     usleep(10000);
-    enc_dec(&amount, &degrees);
-    distance_remaining = fabsf(degrees - target);
+    read_position(&ticks, &cm);
+    distance_remaining = fabsf(cm - target_cm);
   }
   digitalWrite(raise_pin, 0);
   digitalWrite(lower_pin, 0);
-  printf("Target: %f\nActual: %f\n", target, degrees);
+  printf("Target: %.3f cm (%.1f ticks)\nActual: %.3f cm (%d ticks)\n", target_cm,
+         target_cm * TICKS_PER_CM, cm, (int)ticks);
   return 0;
 }
 
