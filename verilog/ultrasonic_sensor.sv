8f06506dc1409e156d1a64eb7b6851e43a735d69
HR-Pi-Admin <instrumentationsubsystem@gmail.com>
2026-04-28 18:37:40 -0700

    Fix ultrasonic sensor FSM and counter logic

--- verilog/ultrasonic_sensor.sv
+++ verilog/ultrasonic_sensor.sv
@@ -4,51 +4,78 @@ module ultrasonic_sensor (
     input logic trigger,
     input logic echo,
     output logic [15:0]distance,
-    output logic trigger_out
+    output logic trigger_out,
+    output logic valid,
+    output logic ready
 );
-    logic [15:0] count_distance;
-    // Keep everything in 1 always_ff block
-    always_ff  @ (posedge clk) begin
-        if (rst | trigger)
-            count_distance <= '0;
-            // Try to use else statements as little as possible
-        else if (echo)
-            count_distance <= count_distance + 1;
-        else 
-            // redundant
-            count_distance <= count_distance;
+
+enum logic [2:0] {IDLE, TRIGGERING, WAITING_FOR_ECHO, SENDING_DISTANCE} ps, ns;
+
+
+logic [15:0] counter;
+
+
+always_ff @ (posedge clk) begin
+    if (rst) begin
+        ps <= IDLE;
+        counter <= '0;
     end
-
-    // Keep this as an integer value and we can use floating point math on the raspberry pi for higher accuracy
-    assign distance = (count_distance * 17) / 1000; // Speed of sound is approximately 0.034 cm/us, divide by 2 for round trip
-
-    logic [3:0]trigger_counter;
-    always_ff @ (posedge clk) begin
-        if (rst) begin
-            trigger_counter <= '0;
-            trigger_out <= 0;
-        end
-
-        else if (trigger) begin
-            trigger_out <= 1;
-            trigger_counter <= '0;
-        end
-
-        // There's no point having this else statement, it'll just add more latency to the hardware
-        else if (trigger_counter > 10) begin
-
-            // Why does this show up twice?
-            trigger_out <= 0;   // Keep trigger high for a short duration
-            trigger_out <= 0;   
-        end 
-        else begin
-            trigger_counter <= trigger_counter+1;
-
-            // This is redundant. Looks like you're trying to do combinational logic
-            trigger_out <= trigger_out;
+    else begin
+        ps <= ns;
+        if (ns != ps && ns != SENDING_DISTANCE) begin
+            counter <= '0;         
+        end else if (ps == TRIGGERING || ps == WAIT_ECHO_HIGH || ps == MEASURE_ECHO) begin
+            counter <= counter + 1; 
         end
     end
+end
 
+always_comb begin 
+    ns = ps;
+    trigger_out = 0;
+    ready = 0;
+    valid = 0;
+    if (ps == IDLE) begin
+        trigger_out = 0;
+        ready = 1;
+        valid = 0;
+        if (trigger)
+            ns = TRIGGERING;
+    end
+    else if (ps == TRIGGERING) begin
+        trigger_out = 1;
+        ready = 0;
+        valid = 0;
+        if (counter > 10) begin
+            trigger_out = 0;
+            if (echo)
+                ns = WAITING_FOR_ECHO;
+            else if (counter > 36000) begin
+                ns = IDLE;
+            end
+        end
+    end
+    else if (ps == WAITING_FOR_ECHO) begin
+        trigger_out = 0;
+        valid = 0;
+        ready = 0;
+        if (!echo) begin
+            ns = SENDING_DISTANCE;
+        end
+        else if (counter > 36000) begin
+            ns = IDLE;
+        end
+    end
+    else if (ps == SENDING_DISTANCE) begin
+        valid = 1;
+        trigger_out = 0;
+        ready = 0;
+        ns = IDLE;
+    end
+
+end
+
+assign distance = counter;
 
 
 endmodule
