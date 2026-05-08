d9ddbcfe2b0978aa948fe20ba96187e32c5e463e
Caleb Ceravolo <ceravcal@uw.edu>
2026-05-08 14:50:53 -0700

    Update pins file to prepare for new load boards

--- C_Code/pins.h
+++ C_Code/pins.h
@@ -11,17 +11,32 @@
 #define LOAD12_3 0 // Centrifuge
 
 #define CENTRIFUGE_PIN LOAD12_3
+#define FAN1_PIN LOAD12_1
+#define FAN2_PIN LOAD12_2
+#define PUMP1_PIN LOAD5_1_1
+#define PUMP2_PIN LOAD5_1_2
+#define PUMP3_PIN LOAD5_2_1
+#define PUMP4_PIN LOAD5_2_2
+#define PUMP5_PIN LOAD5_2_3
+
 // Fluids Pump
 #define H1A_1 5  // Out
 #define H1A_2 4  // In
 
+#define FLUIDSPUMP_IN_PIN H1A_2
+#define FLUIDSPUMP_OUT_PIN H1A_1
 // Spin
 #define H1A_3 1  //
 #define H1A_4 16
 
+#define SPIN_COLUMN_PIN1 H1A_3
+#define SPIN_COLUMN_PIN2 H1A_4
 // Raise Lower Column
 #define H42A_1 8  // Raise
 #define H42A_2 9  // Lower
+
+#define COLUMN_RL_PIN1 H42A_1
+#define COLUMN_RL_PIN2 H42A_2
 
 // Hall effect at column top of travel (wiringPi pin). INPUT + pull-up; wire per your module.
 // Set to -1 to disable the limit in software. AT_TOP is the pin level when the column is at top (0 = LOW).
@@ -34,8 +49,8 @@
 // Raise Lower Pump
 #define H18A_3_1 11 // Lower
 #define H18A_3_2 31 // Raise
-#define LOWER H18A_3_1
-#define RAISE H18A_3_2
+#define LOWER_PUMP_PIN H18A_3_1
+#define RAISE_PUMP_PIN H18A_3_2
 
 #define SDA 8
 #define SCL 9
