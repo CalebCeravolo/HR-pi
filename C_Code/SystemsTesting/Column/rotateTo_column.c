d6b6260e59cf0879528f9d824a6919e561923021
HR-pi-Admin <instrumentationsubsystem@gmail.com>
2026-05-09 15:38:53 -0700

    adjust rotateTo for incremental encoder, need to figure out which pin corresponds to which direction.

--- C_Code/SystemsTesting/Column/rotateTo_column.c
+++ C_Code/SystemsTesting/Column/rotateTo_column.c
@@ -25,12 +25,23 @@ void enc_dec(int32_t *amount, float *degrees) {
 int rotateTo(float target, int left, int right) {
   pinMode(left, OUTPUT);
   pinMode(right, OUTPUT);
-  // int dir;
+  
   int32_t amount;
-  float degrees;
-  enc_dec(&amount, &degrees);
-  float distance_remaining = fabsf((degrees) - (target));
-  if (degrees > target) {
+  float current_degrees;
+  enc_dec(&amount, &current_degrees);
+
+  // For an incremental encoder, find the shortest path to the target angle
+  float diff = fmodf(target - current_degrees, 360.0f);
+  if (diff > 180.0f) {
+    diff -= 360.0f;
+  } else if (diff < -180.0f) {
+    diff += 360.0f;
+  }
+  
+  float goal_degrees = current_degrees + diff;
+  float distance_remaining = fabsf(current_degrees - goal_degrees);
+
+  if (current_degrees > goal_degrees) {
     digitalWrite(left, 0);
     digitalWrite(right, 1);
   } else {
@@ -38,19 +49,20 @@ int rotateTo(float target, int left, int right) {
     digitalWrite(right, 0);
   }
 
-  while ((distance_remaining) > 5) {
+  while (distance_remaining > 5.0f) {
     if (sigint) {
       digitalWrite(left, 0);
       digitalWrite(right, 0);
       break;
     }
     usleep(10000);
-    enc_dec(&amount, &degrees);
-    distance_remaining = fabsf((degrees) - (target));
+    enc_dec(&amount, &current_degrees);
+    distance_remaining = fabsf(current_degrees - goal_degrees);
   }
+  
   digitalWrite(left, 0);
   digitalWrite(right, 0);
-  printf("Target: %f\nActual: %f\n", target, degrees);
+  printf("Target Goal: %f\nActual: %f\n", goal_degrees, current_degrees);
   return 0;
 }
 
