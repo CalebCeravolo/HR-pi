c277dce3089e9092d67989860943a286d5852702
HR-Pi-Admin <instrumentationsubsystem@gmail.com>
2026-05-16 14:57:06 -0700

    Updated stream_spectra

--- C_Code/Webcam/Display/stream_spectra.cpp
+++ C_Code/Webcam/Display/stream_spectra.cpp
@@ -12,26 +12,29 @@
 #include <vector>
 #include <string>
 #include <iostream>
-
-std::vector<double> load_calibration(const std::string& filename) {
-    std::vector<double> data;
-    std::ifstream file(filename);
+#include <deque>
+#include <numeric>
+#include <cmath>
+using namespace std;
+vector<double> load_calibration(const string& filename) {
+    vector<double> data;
+    ifstream file(filename);
 
     if (!file.is_open()) {
-        std::cerr << "Failed to open calibration file: " << filename << "\n";
+        cerr << "Failed to open calibration file: " << filename << "\n";
         return data;
     }
 
-    std::string line;
-    if (std::getline(file, line)) {
-        std::stringstream ss(line);
-        std::string value;
+    string line;
+    if (getline(file, line)) {
+        stringstream ss(line);
+        string value;
 
-        while (std::getline(ss, value, ',')) {
+        while (getline(ss, value, ',')) {
             try {
-                data.push_back(std::stoi(value));
+                data.push_back(stoi(value));
             } catch (...) {
-                std::cerr << "Invalid number in calibration file\n";
+                cerr << "Invalid number in calibration file\n";
             }
         }
     }
@@ -79,13 +82,13 @@ int main(int argc, char** argv) {
     cap.set(cv::CAP_PROP_CONVERT_RGB, 0);
     
     if (!cap.isOpened()) {
-        std::cout << "Camera not opened\n";
+        cout << "Camera not opened\n";
         return -1;
     }
     cv::Mat frame;
     const uint32_t width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
     const uint32_t height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
-    std::vector<int> column_data(width, 0);
+    vector<int> column_data(width, 0);
     uint8_t* row;
     int scale = 1;
     int plot_h = 540;
@@ -99,13 +102,15 @@ int main(int argc, char** argv) {
     const uint32_t scaled_height = (uint32_t)(window_scailing*height);
     printf("%i, %i, %i, %i\n", scaled_width, scaled_height, width, height);
     cv::Mat resized_image(cv::Size(scaled_width, scaled_height),CV_8UC1);
-    std::vector<double> dark_profile = load_calibration("dark_calibration.csv");
-    std::vector<double> light_profile = load_calibration("light_calibration.csv");
+    vector<double> dark_profile = load_calibration("dark_calibration.csv");
+    vector<double> light_profile = load_calibration("light_calibration.csv");
     //Subtract out dark value from MAX value
-    for (int kk=0; kk<width; kk++) {
-        light_profile[kk] -= dark_profile[kk];
-    }
+    // for (int kk=0; kk<width; kk++) {
+    //     light_profile[kk] -= dark_profile[kk];
+    // }
     int margin_left = 60;
+    int counter = 0;
+    int offset = 30;
     while(true) {
         
         cap >> frame;
@@ -141,7 +146,7 @@ int main(int argc, char** argv) {
 
 
         // cv::Mat output_im(height, width, CV_32FC1, );
-        // std::vector<std::vector<int>> output_im(height, std::vector<int>(width));
+        // vector<vector<int>> output_im(height, vector<int>(width));
         // for (int y = 0; y < height; y++) {
         //     row = output_im.ptr<uint8_t>(y);
         //     for (int x = 0; x < width; x++) {
@@ -151,8 +156,10 @@ int main(int argc, char** argv) {
 
         // ticks
         for (int i = 0; i <= 5; i++) {
+            // int percent = i * 20;
             int percent = i * 20;
-            int y = 510 - (percent * 410 / 100); //Set top/bottom bounds for y-axis tick marks
+            double y = (plot_h - offset) - (percent * (plot_h - 2*offset) / 100); //Set top/bottom bounds for y-axis tick marks //1.3 JUST WORKS 
+            //double y = (plot_h - offset)()
 
             cv::line(plot,
                 cv::Point(margin_left - 5, y),
@@ -160,25 +167,93 @@ int main(int argc, char** argv) {
                 cv::Scalar(0), 1);
 
             cv::putText(plot,
-                std::to_string(percent),
+                to_string(percent),
                 cv::Point(5, y + 5),
                 cv::FONT_HERSHEY_SIMPLEX,
                 0.4,
                 cv::Scalar(0), 1);
         }
         
+        int window = 10;
+        deque<double> y1_buffer, y2_buffer;
         for(int x=1;x<width;x++) {
 
-            int y1 = (plot_h)-((column_data[x-1]-dark_profile[x-1])*(plot_h - 100))/light_profile[x-1]; //Divide by ~255
-            int y2 = (plot_h)-((column_data[x]-dark_profile[x])*(plot_h - 100))/light_profile[x];
+            // double y1 = (plot_h)-((column_data[x-1]-dark_profile[x-1])*(plot_h - 2* offset))/(light_profile[x-1]-dark_profile[x-1]); 
+            // double y2 = (plot_h)-((column_data[x]-dark_profile[x])*(plot_h - 2*offset))/(light_profile[x]-dark_profile[x]);
 
+            // if (counter % 10000 == 0){
+            //     cout <<y1<< ": y1"<<'\n';
+            //     cout <<column_data[x-1] << "columndata" <<'\n';
+            //     // cout <<dark_profile[x-1] << "dark" <<'\n';
+            //     cout <<light_profile[x-1]-dark_profile[x-1] << "light" <<'\n';
+            //     cout << (column_data[x-1]-dark_profile[x-1]) << ": ratio" <<'\n';
+
+            // }
+            double y1 = (light_profile[x-1] - dark_profile[x-1])/(column_data[x-1]-dark_profile[x-1]); //Finding percentage of max light
+            double y2 = (light_profile[x-1] - dark_profile[x-1])/(column_data[x-1]-dark_profile[x-1]); //Divide by ~255
+            if ((isinf(y2)) || (y2 <= 0)) {
+                y1 = 100;
+            }
+            if ((isinf(y2)) || (y2 <= 0)) {
+                y2 = 100;
+            }
+            if (counter % 10000 == 0){
+                //cout <<y1<< ": y1"<<'\n';
+                //cout <<column_data[x-1] << "columndata" <<'\n';
+                // cout <<dark_profile[x-1] << "dark" <<'\n';
+                cout <<(light_profile[x-1]-dark_profile[x-1]) << " light" <<'\n';
+                cout << (column_data[x-1]-dark_profile[x-1]) << ": current light" <<'\n';
+                cout << y1 << ": ratio" <<'\n';
+
+            }
+            //add buffers
+            y1_buffer.push_back(y1);
+            y2_buffer.push_back(y2);
+            if (y1_buffer.size() > window) y1_buffer.pop_front();
+            if (y2_buffer.size() > window) y2_buffer.pop_front();
+
+
+            //compute averages
+            double y1_average = accumulate(y1_buffer.begin(), y1_buffer.end(), 0.0) / y1_buffer.size();
+            double y2_average = accumulate(y2_buffer.begin(), y2_buffer.end(), 0.0) / y2_buffer.size();
             
+            // if (counter % 10000 == 0){
+            //     cout <<y1_average<< ": y1avg pre log"<<'\n';
+
+            // }
+
+            double absorb1 = log10(y1_average);
+            double absorb2 = log10(y2_average);
+
+            // if (counter % 10000 == 0){
+            //      cout <<absorb1<< ": abs1"<<'\n';
+
+            // }
+
+            y1_average = plot_h - absorb1*(plot_h - 2*offset)/2;
+            y2_average = plot_h - absorb1*(plot_h - 2*offset)/2;
+
+
+            y1_average -= offset;
+            y2_average -= offset;
+            counter++;
+            if (counter % 10000 == 0){
+                cout <<y1_average<< ": y1avg post offset"<<'\n';
+
+            }
 
             cv::line(plot,
-                    cv::Point((x-1),y1 - 30),
-                    cv::Point((x),y2 - 30),    
-                    cv::Scalar(0,0,0),1);      
-        }
+                    cv::Point((x-1),(int)(y1_average)),
+                    cv::Point((x),(int)(y2_average)),    
+                    cv::Scalar(0,0,0),1);  
+
+
+             }
+        //     cv::line(plot,
+        //             cv::Point((x-1),(int)(y1_average - offset)),
+        //             cv::Point((x),(int)(y2_average - offset)),    
+        //             cv::Scalar(0,0,0),1);      
+        // }
         // cv::Mat img(100, 100, CV_32FC1, data);
         // cv::imwrite("out.tiff", output_im);
         // cv::imwrite("plot.png", plot);
@@ -186,7 +261,7 @@ int main(int argc, char** argv) {
             cv::resize(plot, resized_image, cv::Size(scaled_width, scaled_height));
             cv::imshow("Frame", resized_image);
             
-            std::fill(column_data.begin(), column_data.end(), 0);
+            fill(column_data.begin(), column_data.end(), 0);
             plot.setTo(255);
             if(cv::waitKey(1) == 'q')
                 break;
